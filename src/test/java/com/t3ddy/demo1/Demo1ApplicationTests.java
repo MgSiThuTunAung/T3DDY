4f5256a9cd5f1d8b9329e354a376d7e7fdf90332
MgSiThuTunAung <sithutuna@gmail.com>
2023-10-26 15:54:23 +0630

    first commit

---
+++ src/test/java/com/t3ddy/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t3ddy.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
